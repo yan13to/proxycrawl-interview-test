bf02295c813c46cee697f755e69fb022c5cfef02
Yanto Daryanto <yan13to@gmail.com>
2021-01-27 20:55:44 +0700

    improve authentication from token

--- app/controllers/api/application_controller.rb
+++ app/controllers/api/application_controller.rb
@@ -2,16 +2,25 @@
 
 module Api
   class ApplicationController < ActionController::API
-    before_filter :authenticate_user_from_token!
+    before_action :authenticate_user_from_token!
+    helper_method :current_user
+
+    def current_user
+      @current_user
+    end
 
     private
 
     def authenticate_user_from_token!
-      user_email = params[:user_email].presence
+      user_email = params[:email].presence
       user = user_email && User.find_by_email(user_email)
 
-      if user && Devise.secure_compare(user.authentication_token, params[:user_token])
+      if user && Devise.secure_compare(user.authentication_token, params[:token])
+        @current_user = user
         sign_in user, store: false
+      else
+        @current_user = nil
+        render json: { errors: 'Unauthorized' }, status: :unauthorized
       end
     end
   end
