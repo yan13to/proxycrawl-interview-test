0017e7ff3d42fcea1479db2d675996eb304c4401
Yanto Daryanto <yan13to@gmail.com>
2021-01-28 08:03:23 +0700

    fix missing schemal

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_26_110018) do
+ActiveRecord::Schema.define(version: 2021_01_28_005206) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -48,6 +48,13 @@ ActiveRecord::Schema.define(version: 2021_01_26_110018) do
     t.string "rating"
     t.text "price"
     t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "urls", force: :cascade do |t|
+    t.string "name"
+    t.datetime "scheduled_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
